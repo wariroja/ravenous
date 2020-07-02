3a492683867896b3f6be76e7eec5f6b990831e03
Warinya <wariroja@gmail.com>
2020-07-01 18:00:55 -0700

    Styling: Business PreJSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+/** @format */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '/index.css';
+import App from 'components/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
