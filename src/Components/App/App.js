81d5abfcc629d0b8e55176e13ac1e99f32b712c4
Warinya <wariroja@gmail.com>
2020-07-05 16:18:23 -0700

    Func: props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,26 +1,34 @@
 /** @format */
 
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Codeacademy</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let business = {
+  imageSrc:
+    'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+let businesses = [business, business, business, business, business, business];
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Business/Business.js
+++ src/Components/Business/Business.js
@@ -1,43 +1,30 @@
 /** @format */
 import React from 'react';
-
-let business = {
-  imageSrc:
-    'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-};
+import './Business.css';
 
 class Business extends React.Component {
   render() {
     return (
-      <div class='Business'>
-        <div class='image-container'>
-          <img
-            src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg'
-            alt=''
-          />
+      <div className='Business'>
+        <div className='image-container'>
+          <img src={this.props.business.imageSrc} alt='' />
         </div>
-        <h2>MarginOtto Pizzeria</h2>
-        <div class='Business-information'>
-          <div class='Business-address'>
-            <p>1010 Paddington Way</p>
-            <p>Bordertown</p>
-            <p>NY 10101</p>
+        <h2>{this.props.business.name}</h2>
+        <div className='Business-information'>
+          <div className='Business-address'>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.town}</p>
+            <p>{(this.props.business.state, this.props.business.zipCode)}</p>
           </div>
-          <div class='Business-reviews'>
-            <h3>ITALIAN</h3>
-            <h3 class='rating'>4.5 stars}</h3>
-            <p>90 reviews</p>
+          <div className='Business-reviews'>
+            <h3>{this.props.business.category}</h3>
+            <h3 className='rating'>{this.props.business.rating}star</h3>
+            <p>{this.props.business.reviewCount}</p>
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default Business;
